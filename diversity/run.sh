4ad838699caf58587aa94fdd0e0ae77a0552e3c6
Aaron Rosenfeld <ar374@drexel.edu>
2018-04-11 08:47:12 -0400

    add C10 instances to diversity

--- diversity/run.sh
+++ diversity/run.sh
@@ -1,7 +1,7 @@
 set -e
 
 echo "Making FASTA files"
-python make_fasta.py $DB_CONFIG --min-instances $(seq 1 5)
+python make_fasta.py $DB_CONFIG --min-instances $(seq 1 5) 10
 echo "Running rarefaction"
 bash run_rf.sh *.fasta
 echo "Running diversity"
